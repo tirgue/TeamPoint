66ef581ab6531473a0cef4bd67148e29638fd21c
Birane BA <57402199+birane906@users.noreply.github.com>
2020-12-02 20:44:52 +0100

    Add files via upload

---
+++ code/TeamPoint_Prototype_login/src/main/gui/LoginTPoint/src/module-info.java
@@ -0,0 +1,9 @@
+module LoginTPoint {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires mysql.connector.java;
+
+    opens sample;
+}
