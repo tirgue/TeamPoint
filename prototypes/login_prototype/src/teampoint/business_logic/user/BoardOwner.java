7edb8f8f16b0c218d1578104d78b582f58c56a96
Salim_Azh <azharhoussen.salim@gmail.com>
2020-12-03 23:55:30 +0100

    fix warning unsued import

--- prototypes/login_prototype/src/teampoint/business_logic/user/BoardOwner.java
+++ prototypes/login_prototype/src/teampoint/business_logic/user/BoardOwner.java
@@ -3,7 +3,6 @@
  *******************************************************************************/
 package business_logic.user;
 
-import business_logic.user.WorksapceMember;
 // Start of user code (user defined imports)
 
 // End of user code
