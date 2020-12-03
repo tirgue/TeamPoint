582d7d33c4cef991d7df77af4c9099c688951cd9
Salim_Azh <azharhoussen.salim@gmail.com>
2020-12-04 00:02:22 +0100

    fix database

--- prototypes/login_prototype/src/teampoint/database/JDBCConnector.java
+++ prototypes/login_prototype/src/teampoint/database/JDBCConnector.java
@@ -3,13 +3,9 @@
  *******************************************************************************/
 package database;
 
-import java.sql.DriverManager;
-
-import database.Connection;
+//import java.sql.DriverManager;
+import java.sql.Connection;
 import database.JDBCConnector;
-// Start of user code (user defined imports)
-
-// End of user code
 
 /**
  * Description of JDBCConnector.
@@ -20,35 +16,30 @@ public class JDBCConnector {
 	/**
 	 * Description of the property connection.
 	 */
-	public Connection connection = null;
+	public Connection connection;
 
 	private JDBCConnector() {
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost/TeamPoint?" +
-            	                                   "user=root&password=");
+		/*Class.forName("com.mysql.cj.jdbc.Driver");
+		this.connection = DriverManager.getConnection("jdbc:mysql://localhost/TeamPoint?" + "user=root&password=");*/
 
 	}
-
+	
 	/** Holder */
-    private static class JDBCCOnnectorHolder
-    {       
-        /** Instance unique non préinitialisée */
-        private final static JDBCConnector INSTANCE = new JDBCConnector();
-    }
+	private static class JDBCCOnnectorHolder {
+		/** Instance unique non préinitialisée */
+		private final static JDBCConnector INSTANCE = new JDBCConnector();
+	}
 
 	/** Point d'accès pour l'instance unique du singleton */
-    public static Singleton getInstance()
-    {
-        return JDBCCOnnectorHolder.INSTANCE;
-    }
+	public static JDBCConnector getInstance() {
+		return JDBCCOnnectorHolder.INSTANCE;
+	}
 
-	// Start of user code (user defined methods for JDBCConnector)
-
-	// End of user code
 	/**
 	 * Returns connection.
-	 * @return connection 
+	 * 
+	 * @return connection
 	 */
 	public Connection getConnection() {
 		return this.connection;
